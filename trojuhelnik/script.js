d0172bd7eca16703615de3eac9651130a2c60a9a
TheWallovec <matuswall@gmail.com>
2019-06-29 19:12:55 +0200

    skupA

--- trojuhelnik/script.js
+++ trojuhelnik/script.js
@@ -23,8 +23,11 @@ function calc() {
     }
   }
 
+  // DOPLŇOVÁNÍ STRAN
+
   if(knownVariables>2){
-    if(values.a != "" && values.b != "" && values.gamma != ""){ //A B GAMMA
+    var pokracovat = true;
+    if(values.a != "" && values.b != "" && values.gamma != "" && pokracovat){ //A B GAMMA
       values.c = Math.sqrt(Math.pow(values.a,2)+Math.pow(values.b,2)-2*values.a*values.b*Math.cos(toRadians(values.gamma)));
       values.alpha = toDegrees(Math.asin((values.a*Math.sin(toRadians(values.gamma))/values.c)));
       values.beta = toDegrees(Math.asin((values.b*Math.sin(toRadians(values.gamma))/values.c)));
@@ -32,7 +35,9 @@ function calc() {
       document.forms["strany"]["C"].value = values.c;
       document.forms["uhly"]["ALPHA"].value = values.alpha;
       document.forms["uhly"]["BETA"].value = values.beta;
-    } else if(values.a != "" && values.beta != "" && values.c != ""){ //A BETA C
+
+      pokracovat = false;
+    } else if(values.a != "" && values.beta != "" && values.c != "" && pokracovat){ //A BETA C
       values.b = Math.sqrt(Math.pow(values.a,2)+Math.pow(values.c,2)-2*values.a*values.c*Math.cos(toRadians(values.gamma)));
       values.alpha = toDegrees(Math.asin((values.a*Math.sin(toRadians(values.gamma))/values.c)));
       values.gamma = toDegrees(Math.asin((values.c*Math.sin(toRadians(values.gamma))/values.b)));
@@ -40,15 +45,19 @@ function calc() {
       document.forms["strany"]["B"].value = values.b;
       document.forms["uhly"]["ALPHA"].value = values.alpha;
       document.forms["uhly"]["GAMMA"].value = values.gamma;
-    } else if(values.a != "" && values.beta != "" && values.gamma != ""){ //A BETA GAMMA
+
+      pokracovat = false;
+    } else if(values.a != "" && values.beta != "" && values.gamma != "" && pokracovat){ //A BETA GAMMA
       values.alpha = 180 - values.beta - values.gamma;
       values.b = (values.a * Math.sin(toRadians(values.beta)))/Math.sin(toRadians(values.alpha));
       values.c = (values.a * Math.sin(toRadians(values.gamma)))/Math.sin(toRadians(values.alpha));
-
+      
       document.forms["strany"]["B"].value = values.b;
       document.forms["uhly"]["ALPHA"].value = values.alpha;
       document.forms["strany"]["C"].value = values.c;
-    } else if(values.alpha != "" && values.beta != "" && values.c != ""){ //ALPHA BETA C
+
+      pokracovat = false;
+    } else if(values.alpha != "" && values.beta != "" && values.c != "" && pokracovat){ //ALPHA BETA C
       values.gamma = 180 - values.beta - values.alpha;
       values.b = (values.c * Math.sin(toRadians(values.beta)))/Math.sin(toRadians(values.gamma));
       values.a = (values.c * Math.sin(toRadians(values.alpha)))/Math.sin(toRadians(values.gamma));
@@ -56,7 +65,9 @@ function calc() {
       document.forms["strany"]["B"].value = values.b;
       document.forms["uhly"]["GAMMA"].value = values.gamma;
       document.forms["strany"]["A"].value = values.a;
-    } else if(values.alpha != "" && values.b != "" && values.gamma != ""){ //ALPHA B GAMMA
+
+      pokracovat = false;
+    } else if(values.alpha != "" && values.b != "" && values.gamma != "" && pokracovat){ //ALPHA B GAMMA
       values.beta = 180 - values.alpha - values.gamma;
       values.c = (values.b * Math.sin(toRadians(values.gamma)))/Math.sin(toRadians(values.beta));
       values.a = (values.b * Math.sin(toRadians(values.alpha)))/Math.sin(toRadians(values.beta));
@@ -64,7 +75,9 @@ function calc() {
       document.forms["strany"]["C"].value = values.c;
       document.forms["uhly"]["BETA"].value = values.beta;
       document.forms["strany"]["A"].value = values.a;
-    } else if(values.alpha != "" && values.b != "" && values.c != ""){ //ALPHA B C
+
+      pokracovat = false;
+    } else if(values.alpha != "" && values.b != "" && values.c != "" && pokracovat){ //ALPHA B C
       values.a = Math.sqrt(Math.pow(values.a, 2)+Math.pow(values.c, 2)-2*values.a*values.c*Math.cos(toRadians(values.gamma)));
       values.beta = toDegrees(Math.asin((Math.sin(toRadians(values.alpha))*values.b)/values.a));
       values.gamma = toDegrees(Math.asin((values.c*Math.sin(toRadians(values.beta))/values.b)));
@@ -72,36 +85,70 @@ function calc() {
       document.forms["strany"]["A"].value = values.a;
       document.forms["uhly"]["BETA"].value = values.beta;
       document.forms["uhly"]["GAMMA"].value = values.gamma;
+
+      pokracovat = false;
     }
-    var skupinaA = values.a != "" && values.alpha != "";
-    if(skupinaA && values.b != ""){
+    var skupinaA = values.a != "" && values.alpha != "" && pokracovat;
+    if(skupinaA && values.b != ""){ //A B
+      values.beta = toDegrees(Math.asin((values.b*Math.sin(toRadians(values.alpha))/values.a)));
+      values.gamma = 180 - values.alpha - values.beta;
+      values.c = (values.a*Math.sin(toRadians(values.gamma)))/Math.sin(toRadians(values.alpha));
 
-    } else if(skupinaA && values.beta != ""){
+      document.forms["strany"]["C"].value = values.c;
+      document.forms["uhly"]["BETA"].value = values.beta;
+      document.forms["uhly"]["GAMMA"].value = values.gamma;
 
-    } else if(skupinaA && values.c != ""){
+      pokracovat = false;
+    } else if(skupinaA && values.beta != ""){ //A BETA
+      values.b = (values.a*Math.sin(toRadians(values.beta)))/Math.sin(toRadians(values.alpha));
+      values.gamma = 180 - values.alpha - values.beta;
+      values.c = (values.a*Math.sin(toRadians(values.gamma)))/Math.sin(toRadians(values.alpha));
 
-    } else if(skupinaA && values.gamma != ""){
+      document.forms["strany"]["B"].value = values.b;
+      document.forms["strany"]["C"].value = values.c;
+      document.forms["uhly"]["GAMMA"].value = values.gamma;
 
+      pokracovat = false;
+    } else if(skupinaA && values.c != ""){ //A C
+      values.gamma = toDegrees(Math.asin((values.c*Math.sin(toRadians(values.alpha))/values.a)));
+      values.beta = 180 - values.alpha - values.gamma;
+      values.b = (values.a*Math.sin(toRadians(values.beta)))/Math.sin(toRadians(values.alpha));
+
+      document.forms["uhly"]["BETA"].value = values.beta;
+      document.forms["uhly"]["GAMMA"].value = values.gamma;
+      document.forms["strany"]["B"].value = values.b;
+
+      pokracovat = false;
+    } else if(skupinaA && values.gamma != ""){ // A GAMMA
+      values.c = (values.a*Math.sin(toRadians(values.gamma)))/Math.sin(toRadians(values.alpha));
+      values.beta = 180 - values.alpha - values.gamma;
+      values.b = (values.a*Math.sin(toRadians(values.beta)))/Math.sin(toRadians(values.alpha));
+
+      document.forms["strany"]["B"] = values.b;
+      document.forms["strany"]["C"] = values.c;
+      document.forms["uhly"]["BETA"]  = values.beta;
+
+      pokracovat = false;
     }
-    var skupinaB = values.b != "" && values.beta != "";
+    var skupinaB = values.b != "" && values.beta != "" && pokracovat;
     if(skupinaB && values.a != ""){
-
+      pokracovat = false;
     } else if(skupinaB && values.alpha != ""){
-
+      pokracovat = false;
     } else if(skupinaB && values.c != ""){
-
+      pokracovat = false;
     } else if(skupinaB && values.gamma != ""){
-
+      pokracovat = false;
     }
-    var skupinaC = values.c != "" && values.gamma != "";
+    var skupinaC = values.c != "" && values.gamma != "" && pokracovat;
     if(skupinaC && values.a != ""){
-
+      pokracovat = false;
     } else if(skupinaC && values.alpha != ""){
-
+      pokracovat = false;
     } else if(skupinaC && values.b != ""){
-
+      pokracovat = false;
     } else if(skupinaC && values.beta != ""){
-
+      pokracovat = false;
     }
   }
 }
